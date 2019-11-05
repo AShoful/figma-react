0ac39085f2d52c259b3c042b81bc0c4141ac3d46
Angrey Shoful <shoful.a@gmail.com>
2019-11-05 12:36:43 +0200

    fixed welcome

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import ReactDependentScript from 'react-dependent-script'
 
 import { connect } from 'react-redux';
 
 import styles from './Welcome.module.css';
 
-const COORDS_PELETEX = [50.5443651, 30.6536747];
+import { COORDS_PELETEX } from '../../constants/constants';
 
 const TEXTS = {
     ua: {
@@ -31,29 +30,17 @@ class Welcome extends Component {
         lang: PropTypes.string
     };
 
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            latitude: COORDS_PELETEX[0],
-            longitude: COORDS_PELETEX[1]
-        };
-    }
-
     componentDidMount () {
-        setTimeout(()=> window.google ? this.initMap() : alert('hhggjgj'), 0);
+        setTimeout(()=> window.google ? this.initMap() : alert('Не удалось подключить карту'), 0);
     }
 
     initMap () {
-        const directionsRenderer = new window.google.maps.DirectionsRenderer();
-        const directionsService = new window.google.maps.DirectionsService();
         const mapOptions = {
             zoom: 10,
             center: { lat: COORDS_PELETEX[0], lng: COORDS_PELETEX[1] }
         };
         const map = new window.google.maps.Map(document.getElementById('map'), mapOptions);
-        directionsRenderer.setMap(map);
-
+        
         const marker = new window.google.maps.Marker({
             position: { lat: COORDS_PELETEX[0], lng: COORDS_PELETEX[1] },
             label: { text: 'Pelletex', color: '#463285' },
@@ -62,29 +49,27 @@ class Welcome extends Component {
 
         marker.setMap(map);
 
-        document.getElementById('submit').addEventListener('click', (e) => {
-            e.preventDefault();
+        document.getElementById('submit').addEventListener('click', () => {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(
                     position => {
                         const { latitude, longitude } = position.coords;
-                        this.setState({
-                            latitude,
-                            longitude
-                        }, () => this.calculateAndDisplayRoute(directionsService, directionsRenderer));
+                        this.calculateAndDisplayRoute(latitude, longitude, map)
                     },
                     error => {
-                        alert('Ваш браузер блокирует передачу данных для построения маршрута');
+                        window.alert(error.message + 'Ваш браузер блокирует передачу данных для построения маршрута');
                     });
             } else { alert('Ваш браузер не поддерживает сервис построения маршрута'); }
         });
     }
 
-    calculateAndDisplayRoute = (directionsService, directionsRenderer) => {
-        const { latitude, longitude } = this.state;
-        const origin = { lat: latitude, lng: longitude };
+    calculateAndDisplayRoute = (lat, lng, map) => {
+        const directionsRenderer = new window.google.maps.DirectionsRenderer();
+        const directionsService = new window.google.maps.DirectionsService();
+        directionsRenderer.setMap(map);
+        
         directionsService.route({
-            origin,
+            origin:  { lat, lng },
             destination: { lat: COORDS_PELETEX[0], lng: COORDS_PELETEX[1] },
             travelMode: 'DRIVING'
         }, function (response, status) {
@@ -98,12 +83,12 @@ class Welcome extends Component {
 
     render () {
         const { lang } = this.props;
-        
+                
         return <div className={styles.welcome} >
             <div className={styles.title}>{TEXTS[lang].title}</div>
             <div className={styles.wrap}>
                 <div className={styles.address}>
-                    <button className={styles.addressButton} id='submit' >
+                    <button className={styles.addressButton} id='submit'>
                         {TEXTS[lang].titleButton}
                     </button>
                     <div className={styles.addressPlace}> {TEXTS[lang].address}</div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
 export const LANGS = ['ua', 'ru']; 
 export const DEFAULT_LANG = LANGS[0];
 export const GOOGLE_MAPS_KEY = 'AIzaSyAiwLyPsExhVdhK6jJI-eYMeEFkSvkHHdc';
+export const COORDS_PELETEX = [50.5443651, 30.6536747];
