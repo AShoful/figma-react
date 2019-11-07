7c448b26ec2ce9bcdea5dbe551beaa84ff9ecf0e
Angrey Shoful <shoful.a@gmail.com>
2019-11-07 22:01:02 +0200

    Updates

---
+++ precache-manifest.6723e6248a99f961eb7bac7969050ab4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0877fa650b683de3dfea456d4e97f14",
+    "url": "/figma-react/index.html"
+  },
+  {
+    "revision": "997fa7f6473f4e859c24",
+    "url": "/figma-react/static/css/main.6f108d2f.chunk.css"
+  },
+  {
+    "revision": "2117581fad43d5ce45f9",
+    "url": "/figma-react/static/js/2.fd619bd0.chunk.js"
+  },
+  {
+    "revision": "997fa7f6473f4e859c24",
+    "url": "/figma-react/static/js/main.20edf4b1.chunk.js"
+  },
+  {
+    "revision": "8728077aca8e07707e4c",
+    "url": "/figma-react/static/js/runtime-main.3f2c5e5f.js"
+  },
+  {
+    "revision": "50475e9398305457c4ead55941b10a0f",
+    "url": "/figma-react/static/media/HelveticaNeueCyr-Bold.50475e93.ttf"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "/figma-react/static/media/HelveticaNeueCyr-Light.30778183.ttf"
+  },
+  {
+    "revision": "e603c690f754c70654004a753c17b60b",
+    "url": "/figma-react/static/media/HelveticaNeueCyr-Medium.e603c690.ttf"
+  },
+  {
+    "revision": "0b1e500de0ac51dc7a2e0d07e5121578",
+    "url": "/figma-react/static/media/Rectangle.0b1e500d.png"
+  },
+  {
+    "revision": "bcd38194cae0930af3bd2c08f9c00256",
+    "url": "/figma-react/static/media/Rectangle.bcd38194.png"
+  },
+  {
+    "revision": "49a4ae60801480c530fea25eb9cb6953",
+    "url": "/figma-react/static/media/RectangleMobile.49a4ae60.png"
+  },
+  {
+    "revision": "79aa600424ded269e432988cb556d283",
+    "url": "/figma-react/static/media/Rubik-Light.79aa6004.ttf"
+  },
+  {
+    "revision": "35af6dbe749b9704f03282e5a5949253",
+    "url": "/figma-react/static/media/Rubik-Regular.35af6dbe.ttf"
+  },
+  {
+    "revision": "a21da0f98cb89e4da84d9ae8a87bb8f1",
+    "url": "/figma-react/static/media/bag.a21da0f9.png"
+  },
+  {
+    "revision": "517027b597f74d30a840d43f75034a13",
+    "url": "/figma-react/static/media/bags.517027b5.png"
+  },
+  {
+    "revision": "5e83ec6ac8f7b13a4371c23f15f34cdc",
+    "url": "/figma-react/static/media/slide1.5e83ec6a.jpg"
+  },
+  {
+    "revision": "5a7300727db2be4f48e41ad40291d5bb",
+    "url": "/figma-react/static/media/slide2.5a730072.png"
+  },
+  {
+    "revision": "a264917fb98f8f8b345679541c841f52",
+    "url": "/figma-react/static/media/slide3.a264917f.png"
+  },
+  {
+    "revision": "0c92af4c72c562eb9fdf460d736a88dd",
+    "url": "/figma-react/static/media/slide4.0c92af4c.png"
+  }
+]);
